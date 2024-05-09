cce7e193305b4bd96a1adc4112aab63eabaf1104
Bogdan <bogdan.pettrovic@gmail.com>
2024-05-09 18:33:03 +0200

    Add project files.

---
+++ 2018 pokusaj 1/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2018_pokusaj_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            // n mora biti 6 (kao u primeru na sajtu) jer ne znam sta bih stavio da oni unesu vise vrednosti u jednom elementu stoga sam stavio 6 textboxa
+            int N = int.Parse(textBox7.Text);
+            if (N>=5 && N<50)
+            {
+                int[] a = new int[N];
+                // pokusavao sam da namestim da se automatski stavljaju brojevi u niz ali nisam uspeo, a i na google-u pokazuju nesto sto mi je nepoznato. sa obzirom da mislim da mi
+                // radimo u konzolnim aplikacijama na takmicenju nisam hteo vise da gubim vreme na textbox pa sam samo manuelno postavio
+                a[0] = int.Parse(textBox1.Text);
+                a[1] = int.Parse(textBox2.Text);
+                a[2] = int.Parse(textBox3.Text);
+                a[3] = int.Parse(textBox4.Text);
+                a[4] = int.Parse(textBox5.Text);
+                a[5] = int.Parse(textBox6.Text);
+
+                int x = int.Parse(textBox8.Text);
+                int y = int.Parse(textBox9.Text);
+                int z = int.Parse(textBox10.Text);
+
+                if ((x > -50 && x < 50) && (y >= 1 && y <= 10))
+                {
+                    int[] b = new int[N + 1];
+                    for(int i=0; i<a.Length; i++)
+                    {
+                        b[i] = a[i];
+                    }
+                    b[N] = x;
+                    int prethodnavrednost;
+                    for (int i = 1; i < b.Length; i++)
+                    {
+                        if (b[i - 1] > b[i])
+                        {
+                            prethodnavrednost = b[i - 1];
+                            b[i - 1] = b[i];
+                            b[i] = prethodnavrednost;
+                            i = 1;
+                        }
+                    }
+                    for (int i = 0; i < b.Length; i++)
+                    {
+                        label7.Text = label7.Text + " " + b[i];
+                    }
+
+                }
+                else
+                {
+                    label1.Text = "opseg nije dobar";
+                }
+
+            }
+            else
+            {
+                label1.Text = "n mora imati vrednost od 5 do 49";
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
